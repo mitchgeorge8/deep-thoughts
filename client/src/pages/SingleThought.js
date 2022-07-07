ec142d4780d1107ddbfae0cc59e4153dca3d358e
Mitchell George <mitchgeorge8@gmail.com>
2022-07-07 18:33:21 -0400

    21.4.3 Set up the main url routes using react router

--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React from "react";
 
-const SingleThought = props => {
+const SingleThought = (props) => {
   return (
     <div>
       <div className="card mb-3">
         <p className="card-header">
           <span style={{ fontWeight: 700 }} className="text-light">
             Username
-          </span>{' '}
+          </span>{" "}
           thought on createdAt
         </p>
         <div className="card-body">
